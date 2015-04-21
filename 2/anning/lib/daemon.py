0b9db8b4454083f5d30145d4671dc84d6b938e4a
jerey08 <fuquanshi08@gmail.com>
2015-04-21 15:46:44 +0800

    安宁作业

---
+++ 2/anning/lib/daemon.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#-*-coding:utf8-*-
+
+""" daemon class """
+
+import sys, os, atexit, time
+from signal import SIGTERM
+
+class Daemon:
+
+    """ handle for Daemon """
+
+    def __init__(self, pidfile, Sockfile, Urlsite, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.pidfile = pidfile
+        self.Sockfile = Sockfile
+        self.Urlsite = Urlsite
+
+    def _daemonize(self):
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        os.setsid()
+        os.chdir("/")
+        os.umask(0)
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        atexit.register(self.delpid)
+
+        pid = str(os.getpid())
+        file(self.pidfile, 'w+').write("%s\n" % pid)
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        self._daemonize()
+        self._run()
+
+    def stop(self):
+        try:
+            pf = file(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return # not an error in a restart
+
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def _run(self):
+        pass
+
+
